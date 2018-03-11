648ee0e85289ccd8b301db891c35efa6b0f035e4
Eric Wang <ericlwan@usc.edu>
2018-03-10 18:24:51 -0600

    initialized project with cocoapods and initial pods

---
+++ Mixtape/Pods/Target Support Files/Pods-MixtapeTests/Pods-MixtapeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MixtapeTests : NSObject
+@end
+@implementation PodsDummy_Pods_MixtapeTests
+@end
---
+++ Mixtape/Pods/Target Support Files/Pods-MixtapeUITests/Pods-MixtapeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MixtapeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MixtapeUITests
+@end
